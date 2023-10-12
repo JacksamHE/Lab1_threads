5db398430100a67b9601f7dbc8efdd4b4ef2ec0d
JacksamHE <147696014+JacksamHE@users.noreply.github.com>
2023-10-12 17:30:12 +0800

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -2,7 +2,7 @@
  * author     ：HE XIPING
  * email      ：he.xiping@infiz.khpi.edu.ua
  * date       ：Created in 2023/10/12
- * description：Task3
+ * description：Task1
  */
 #define _CRT_SECURE_NO_WARNINGS
 
